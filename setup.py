8d7ebfc4d114a76746ea514b44ece039e58eb352
AlexStark <alex.stark3038@gmail.com>
2023-08-03 09:51:24 +0300

    Создал пакет для orm client

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+REQUIRES = [
+    'structlog',
+    'sqlalchemy',
+    'allure-pytest'
+]
+
+setup(
+    name='restclient',
+    version='0.0.1',
+    packages=['orm_client'],
+    url='https://github.com/AlexStark9/orm_client.git',
+    license='MIT',
+    author='Alex Stark',
+    author_email='-',
+    install_requires=REQUIRES,
+    description='orm client with allure and loger'
+)
